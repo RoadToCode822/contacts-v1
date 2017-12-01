d4812326a51f71c019fcf2856d9dd9b2a2bebfc7
Mahtab Alam <mahtab822@gmail.com>
2017-12-01 00:26:38 -0500

    create, read and delete functionalities work. Delete operation has been added to routes.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var ObjectID = require('mongodb').ObjectID;
 
 // Data
 var Data = [];
@@ -7,33 +8,45 @@ var Data = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
-  res.render('index', { contacts: Data });
+  req.db.collection('Contacts').find().toArray(function(err, results) {
+    // console.log(results)
+    // send HTML file populated with quotes here
+
+    return res.render('index', { contacts: results });
+  })
 });
 
 router.post('/contacts/addNew', function(req, res, next){
   
-    if (err) return console.log(err)
     const db = req.db;
     db.collection('Contacts').save(req.body, (err, result) => {
       if (err) return console.log(err)
   
       console.log('Saved to database')
-      res.redirect('/');
-      var name = req.body.fullName;
-      var phone = req.body.phoneNumber;
-      var address = req.body.address;
+      
+      // var name = req.body.fullName;
+      // var phone = req.body.phoneNumber;
+      // var address = req.body.address;
     
-      Data.push({name: name, phone: phone, address: address});
-    
-      res.render('index', {contacts: Data});
+      return res.redirect('/');
 
-  
-   
-  
-  });
+    });
     
   });
+
+  router.get('/contacts/delete/:_id', function(req,res){
+    console.log(req.params._id);
+    const db = req.db;
+    var objId = new ObjectID(req.params._id);
+    db.collection('Contacts').deleteOne( { _id: objId }, function(err, result){
+      console.log('foobar');
+      if (err) {
+        console.log(err)
+        return res.send(err)
+      }
+      return res.redirect('/');
+    } )
+  }); 
   
 
 module.exports = router;
